cff666e071c473aafb8f528fba0d86a413368bd1
arzisukajs <github188@gmail.com>
2026-05-13 09:40:21 +0700

    kasir

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Produk p1 = new Elektronik("Samsung", 100000, 5, 2);
         Produk p2 = new Makanan("Indomie", 1000, 100, 3);
+        Kasir kasir = new Kasir("Maman");
     }
 }
