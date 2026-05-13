f75b2de52b2b7183598553a64d94f4d1eb173e63
arzisukajs <github188@gmail.com>
2026-05-13 09:50:32 +0700

    produk java

--- Produk.java
+++ Produk.java
@@ -1,3 +1,34 @@
-public class Produk {
-    
+public abstract class Produk {
+
+    private String nama;
+    private double harga;
+    private int stok;
+
+    public Produk(String nama, double harga, int stok) {
+        this.nama = nama;
+        this.harga = harga;
+        this.stok = stok;
+    }
+
+    public void setStok(int jmlBeli) {
+        stok = stok - jmlBeli;
+    }
+
+    public int getStok() {
+        return stok;
+    }
+
+    public void tampilInfo() {
+        System.out.println("===== Produk =====");
+        System.out.println("Nama: " + nama);
+        System.out.println("Harga: " + harga);
+        System.out.println("Stok: " + stok);
+    }
+
+    public double hitungTotalHarga(int jmlhBeli) {
+        return jmlhBeli * harga;
+    }
+
+    public abstract double hitungHargaAkhir(int jmlhBeli);
+
 }
