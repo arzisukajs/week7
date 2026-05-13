9f4cd8a844a5570abe0fa28980c2b61ff9f89e00
arzisukajs <github188@gmail.com>
2026-05-13 09:49:20 +0700

    kasir

--- Kasir.java
+++ Kasir.java
@@ -1,3 +1,27 @@
 public class Kasir {
-    
+    private String namaKasir;
+
+    public Kasir(String namaKasir) {
+        this.namaKasir = namaKasir;
+    }
+
+    public void tampilIndentitas() {
+        System.out.println("===== TOKO GARUDA =====");
+        System.out.println("Nama Kasir: " + namaKasir);
+    }
+
+    public void prosesBelanja(Produk p, int jmlhBeli) {
+
+        if (p.getStok() < jmlhBeli) {
+            System.out.println("Out of stok");
+        }
+        {
+            double hargaAkhir = p.hitungHargaAkhir(jmlhBeli);
+            System.out.println("Harga Akhir = " + hargaAkhir);
+            p.setStok(jmlhBeli);
+        }
+
+        System.out.println("Stok terbaru=" + p.getStok());
+
+    }
 }
