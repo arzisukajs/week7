4097a1812d4dc3c3e9be3c5d2617de0a186a847e
arzisukajs <github188@gmail.com>
2026-05-13 09:51:23 +0700

    elektronik

--- Elektronik.java
+++ Elektronik.java
@@ -1,3 +1,22 @@
-public class Elektronik {
-    
+public class Elektronik extends Produk {
+
+    private int masaGaransi;
+
+    public Elektronik(String nama, double harga, int stok, int masaGaransi) {
+        super(nama, harga, stok);
+        this.masaGaransi = masaGaransi;
+    }
+
+    @Override
+    public double hitungHargaAkhir(int jmlhBeli) {
+        System.out.println("Total Harga= " + hitungTotalHarga(jmlhBeli));
+        return hitungTotalHarga(jmlhBeli) * 0.85;
+    }
+
+    @Override
+    public void tampilInfo() {
+        super.tampilInfo();
+        System.out.println("Masa Garansi: " + masaGaransi + "Tahun");
+    }
+
 }
