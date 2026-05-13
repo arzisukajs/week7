ef5bfd514a9a721583df7b850e5ef343db86c46f
arzisukajs <github188@gmail.com>
2026-05-13 09:39:58 +0700

    Produk p2

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Produk p1 = new Elektronik("Samsung", 100000, 5, 2);
+        Produk p2 = new Makanan("Indomie", 1000, 100, 3);
     }
 }
